d07db1d82a44bebacadf1c8c25612f7ad4fc7b53
Ness <vanessa.chu@mail.mcgill.ca>
2021-01-01 02:14:15 -0500

    Add scramble, remove background image

---
+++ scramble.js
@@ -0,0 +1,22 @@
+function scramble(){
+    var title = new String(document.getElementById('title').innerText);
+    var temp = new String(title);
+    for (var i=0;i<temp.length-1;i++){
+        var coinFlip = Math.random() >= 0.5;
+        if (coinFlip == false){
+            temp = temp.substr(0,i) + temp.charAt(i).toUpperCase() + temp.slice(i+1);
+        }
+        else{
+            temp = temp.substr(0,i) + temp.charAt(i).toLowerCase() + temp.slice(i+1);
+        }
+    }
+    document.getElementById('title').innerHTML = temp;
+}
+function unscramble(){
+    var title = new String(document.getElementById('title').innerText);
+    var temp = new String(title);
+    for (var i=0;i<title.length-1;i++){
+        temp = temp.substr(0,i) + temp.charAt(i).toLowerCase() + temp.slice(i+1);
+    }
+    document.getElementById('title').innerHTML = temp;
+}
