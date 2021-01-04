ec9003f9aa8707a5f446705c24b6228de98e7e6b
Ness <vanessa.chu@mail.mcgill.ca>
2021-01-03 22:09:54 -0500

    Add icon colours invert. Fix hover losing color change on invert

--- settings.js
+++ settings.js
@@ -1,15 +1,28 @@
 function toggleMode(){
     var all = document.getElementsByTagName("*");
+    var allImages = document.getElementsByTagName("img");
     if (document.body.style.backgroundColor === "white"){
         for (var i=0, max=all.length; i < max; i++) {
+            if (all[i].id.toLowerCase() === "settings-button" || all[i].id.toLowerCase() === "settings-option" || all[i].id.toLowerCase() === "settings") continue;
             all[i].style.color = "white";
             all[i].style.backgroundColor = "black";
+        }
+        for (var i=0, max=allImages.length; i < max; i++) {
+            allImages[i].transition = "0s";
+            allImages[i].src = allImages[i].src.replace("_dark.png", "") + "_light.png";
+            allImages[i].transition = "0.5s";
         }
     }
     else {
         for (var i=0, max=all.length; i < max; i++) {
+            if (all[i].id.toLowerCase() === "settings-button" || all[i].id.toLowerCase() === "settings-option" || all[i].id.toLowerCase() === "settings") continue;
             all[i].style.color = "black";
             all[i].style.backgroundColor = "white";
+        }
+        for (var i=0, max=allImages.length; i < max; i++) {
+            allImages[i].transition = "0s";
+            allImages[i].src = allImages[i].src.replace("_light.png", "") + "_dark.png";
+            allImages[i].transition = "0.5s";
         }
     }
 }
@@ -18,7 +31,7 @@ function changeFontSize(value){
     var all = document.getElementsByTagName("*");
     for (var i=0, max=all.length; i < max; i++) {
         var currSize = window.getComputedStyle(all[i]).fontSize.replace("px", "");
-        var newSize = parseInt(currSize) + parseInt(value);
+        var newSize = parseFloat(currSize) + parseFloat(value);
         all[i].style.fontSize = newSize + "px";
     }
 }
