2c98f9244a941ce47330c035d7b5e91c1ac90e0e
Ness Chu <vc00@ymail.com>
2021-07-22 02:48:37 -0400

    Combine settings scripts.

--- settings.js
+++ settings.js
@@ -1,3 +1,11 @@
+function showSettings() {
+    var settings = document.getElementById("settings-options");
+    if (settings.style.display === "initial"){
+        settings.style.display = "none";
+    }
+    else settings.style.display = "initial";
+}
+
 var lightmode = false;
 
 function toggleMode(){
--- showSettings.js
+++
@@ -1,7 +0,0 @@
-function showSettings() {
-    var settings = document.getElementById("settings-options");
-    if (settings.style.display === "initial"){
-        settings.style.display = "none";
-    }
-    else settings.style.display = "initial";
-}
